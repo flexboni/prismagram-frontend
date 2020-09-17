f0273620ae2dde23f75a0aebf64589bf8c125fff
flexboni <kgb8594@gmail.com>
2020-09-17 11:43:55 +0900

    7.0 Getting the Feed and Loader Component

--- src/Routes/Feed.js
+++ src/Routes/Feed.js
@@ -1,1 +1,45 @@
-export default () => "Feed";
+import React from "react";
+import { useQuery } from "react-apollo-hooks";
+import Loader from "../Components/Loader";
+
+const FEED_QUERY = gql`
+  {
+    seeFeed {
+      id
+      location
+      caption
+      user {
+        id
+        avatar
+        username
+      }
+      files {
+        id
+        url
+      }
+      likeCount
+      isLiked
+      comments {
+        id
+        text
+        user {
+          id
+          username
+        }
+      }
+      createAt
+    }
+  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 80vh;
+`;
+
+export default () => {
+  const { data, loading } = useQuery(FEED_QUERY);
+  return <Wrapper>{loading && <Loader />}</Wrapper>;
+};
